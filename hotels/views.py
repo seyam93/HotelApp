1851a88e9b92b196c0843208282037a247c4d4a9
seyam <seyam@gmail.com>
2025-04-23 01:23:56 +0200

    Second Commit

--- hotels/views.py
+++ hotels/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Hotel
+from .models import Hotel, Room
 
 def home(request):
     hotels = Hotel.objects.all()
@@ -11,4 +11,17 @@ def about(request):
 def hotel_detail(request, slug):
     hotel = get_object_or_404(Hotel, slug=slug)
     return render(request, 'hotels/hotel_detail.html', {'hotel': hotel})
+
+def room_list(request):
+    rooms = Room.objects.all()
+    return render(request, 'hotels/rooms.html', {'rooms': rooms})
+
+def room_detail(request):
+    # Dummy test data
+    room = {
+        'name': 'Ocean View Suite',
+        'description': 'A beautiful suite with a view of the sea.',
+        'price': 350
+    }
+    return render(request, 'hotels/rooms_detail.html', {'room': room})
 
