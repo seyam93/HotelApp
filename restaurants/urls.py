b22277c9a781da75a98f7f41f745eb4963d11a74
seyam <seyam@gmail.com>
2025-04-29 11:03:21 +0300

    29 April

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug:hotel_slug>/restaurants/', views.restaurant_list, name='restaurant-list'),
-    path('<slug:hotel_slug>/restaurants/<slug:slug>/', views.restaurant_detail, name='restaurant-detail'),
+    path('home/<slug:hotel_slug>/restaurants/', views.restaurant_list, name='restaurant-list'),
+    path('home/<slug:hotel_slug>/restaurants/<slug:slug>/', views.restaurant_detail, name='restaurant-detail'),
 ]
