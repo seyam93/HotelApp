53120e37d676b533db252e28d096c76e0ca0b26b
seyam <seyam@gmail.com>
2025-06-15 01:46:43 +0300

    Gallery Update

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import send_career_application
 
 urlpatterns = [
-    path('home/<slug:hotel_slug>/contact', views.hotel_contact_view, name='contact-page'),
-    path('home/<slug:hotel_slug>/about', views.about_page, name='about-page'),
+    path('home/<slug:hotel_slug>/contact/', views.hotel_contact_view, name='contact-page'),
+    path('home/<slug:hotel_slug>/about/', views.about_page, name='about-page'),
     path('home/<slug:hotel_slug>/faqs/', views.hotel_faqs, name='faqs-page'),
     path('home/<slug:hotel_slug>/careers/', views.careers_page, name='careers-page'),
     path('careers/apply/', send_career_application, name='career-apply'),
